424554cc11ede826fe4f5c3e428431fef64d623c
alexarobertss <alexarobertss@protonmail.com>
2022-06-25 19:32:52 -0400

    day 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Router, Routes, Route, Link, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Recipe from './Recipe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      {/* <Recipe/> */}
+    </BrowserRouter>
+    </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
